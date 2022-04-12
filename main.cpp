24a0d4d9156f4d0721a0ff886de10c71440a0eda
namanchauhan <naman0909.be21@chitkara.edu.in>
2022-04-12 16:39:58 +0530

    adding files in main branch

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// this is a master branch
+// 1111111111111111111111111111111111
+// 1111111111111111111111111111111111
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    a=10;
+    b=20;
+    cout<<a+b;
+    return 0;
+}
